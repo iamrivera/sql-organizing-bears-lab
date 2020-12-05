5e9d31bc233c8107dc419e395051c7372dede278
iamrivera <a3m3rivera@gmail.com>
2020-12-04 16:38:12 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 23, "M", "Black", "Grumpy", 0);
+INSERT INTO bears VALUES (2, "Rowdy", 23, "M", "Black", "Grumpy", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 23, "F", "Black", "Grumpy", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 23, "M", "Black", "Grumpy", 0);
+INSERT INTO bears VALUES (5, "Melissa", 23, "F", "Black", "Grumpy", 0);
+INSERT INTO bears VALUES (6, "Grinch", 23, "M", "Black", "Grumpy", 0);
+INSERT INTO bears VALUES (7, "Wendy", 23, "M", "Black", "Grumpy", 0);
+INSERT INTO bears (id, age, alive) VALUES (8, 23, 0);
